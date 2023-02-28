158db9d825ac1a25a7f985f548f6740a0723ec10
francescogentile25 <francescogentile250893@gmail.com>
2023-02-28 16:02:49 +0100

    FEAT AppTitle. vue add

---
+++ vite-hello/src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <h1>{{title}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite'
+        }
+    },
+}
+</script>
+
+<style>
+    h1{
+        color: salmon;
+    }
+</style>
